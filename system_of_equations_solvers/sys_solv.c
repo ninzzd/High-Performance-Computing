2a727880050e1023c05dbd87d912a86c3ce10073
ninzzd <ninaad.desai1212@gmail.com>
2025-08-12 04:40:56 +0530

    Just added simple input/output testing blocks for the initial guess

--- system_of_equations_solvers/sys_solv.c
+++ system_of_equations_solvers/sys_solv.c
@@ -3,7 +3,7 @@
 
 int main(){
     float** a;
-    float* x;
+    float* x_k;
     float* b;
     int n;
     printf("\nEnter the number of variables/equations: ");
@@ -12,7 +12,7 @@ int main(){
     for(int i = 0;i < n;i++){
         a[i] = (float*)malloc(n*sizeof(float));
     }
-    x = (float*)malloc(n*sizeof(float));
+    x_k = (float*)malloc(n*sizeof(float));
     b = (float*)malloc(n*sizeof(float));
     printf("\nEnter the coefficients of the equations (elements of the A matrix): ");
     for(int i = 0;i < n;i++){
@@ -24,6 +24,10 @@ int main(){
     for(int i = 0;i < n;i++){
         scanf("%f",&b[i]);
     }
+    printf("\nEnter the initial guess x_0: ");
+    for(int i = 0;i < n;i++){
+        scanf("%f",&x_k[i]);
+    }
     printf("\nMatrix A:\n");
     for(int i = 0;i < n;i++){
         for(int j = 0;j < n;j++){
@@ -31,9 +35,13 @@ int main(){
         }
         printf("\n");
     }
-        printf("\nMatrix B:\n");
+    printf("\nMatrix B:\n");
     for(int i = 0;i < n;i++){
         printf("%f\n",b[i]);
+    }
+    printf("\nMatrix X:\n");
+    for(int i = 0;i < n;i++){
+        printf("%f\n",x_k[i]);
     }
     return 0;
 }
